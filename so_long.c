2ac56366384ffaa51a5c091153a4cabd9dd502fe
Piotr Biedermann <piotr.biedermann84@gmail.com>
2022-09-27 12:27:00 +0200

    made square

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 11:47:04 by pbiederm          #+#    #+#             */
-/*   Updated: 2022/09/27 11:48:20 by pbiederm         ###   ########.fr       */
+/*   Updated: 2022/09/27 12:25:14 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,41 @@
 
 #include <mlx.h>
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_lenght + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	// void	*img;
+	t_data	img;
+	int	x;
+	int	y;
+
+	y = 5;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lenght, &img.endian);
+	while (y < 100)
+	{
+		x = 5;
+		while (x < 100)
+		{
+			my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+			x++;
+		}
+		my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+		y++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 11:47:21 by pbiederm          #+#    #+#             */
-/*   Updated: 2022/09/27 11:49:12 by pbiederm         ###   ########.fr       */
+/*   Updated: 2022/09/27 11:57:21 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 # define SO_LONG_H
 # include "./libft/libft.h"
 # include "./mlx/mlx.h"
+# include <mlx.h>
 
-#include <mlx.h>
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_lenght;
+	int		endian;
+}t_data;
 
 #endif
