a0ee25b72e4f4354f0f124f5246585c25346381b
Piotr Biedermann <piotr.biedermann84@gmail.com>
2022-09-27 12:50:01 +0200

    three colors

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 11:47:04 by pbiederm          #+#    #+#             */
-/*   Updated: 2022/09/27 12:34:33 by pbiederm         ###   ########.fr       */
+/*   Updated: 2022/09/27 12:47:47 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,10 @@ int	main(void)
 		x = 5;
 		while (x < 100)
 		{
-			my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+			my_mlx_pixel_put(&img, x, y, 0x0000CC00);
 			x++;
 		}
-		my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+		my_mlx_pixel_put(&img, x, y, 0x0000CC00);
 		y++;
 	}
 
@@ -59,9 +59,37 @@ int	main(void)
 	while (i < 100)
 	{
 		my_mlx_pixel_put(&img, x + i, y, 0x00FF0000);
-		my_mlx_pixel_put(&img, x + i, y + i, 0x00FF0000);
-		my_mlx_pixel_put(&img, x + 100, y + i, 0x00FF0000);
+		my_mlx_pixel_put(&img, x + i, y + i, 0x00F00000);
+		my_mlx_pixel_put(&img, x + 100, y + i, 0x00F70000);
 		i++;
+	}
+
+	x = 405;
+	y = 5;
+	while (y < 100)
+	{
+		x = 405;
+		while (x < 500)
+		{
+			my_mlx_pixel_put(&img, x, y, 0x00FFFF00);
+			x++;
+		}
+		my_mlx_pixel_put(&img, x, y, 0x00FFFF00);
+		y++;
+	}
+
+	x = 605;
+	y = 5;
+	while (y < 100)
+	{
+		x = 605;
+		while (x < 700)
+		{
+			my_mlx_pixel_put(&img, x, y, 0x000000CC);
+			x++;
+		}
+		my_mlx_pixel_put(&img, x, y, 0x000000CC);
+		y++;
 	}
 
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
